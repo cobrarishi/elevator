a3cbb1dfe04d18447edbf3bca00a801f77e60976
cobrarishi <88015214+cobrarishi@users.noreply.github.com>
2025-11-16 16:08:55 -0600

    Add files via upload

---
+++ src/client/java/coaltrain/elevator/ElevatorClient.java
@@ -0,0 +1,10 @@
+package coaltrain.elevator;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElevatorClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
