e8f033e02daa09e9a50081501835ae0e1eb33626
cobrarishi <88015214+cobrarishi@users.noreply.github.com>
2025-11-16 21:11:48 -0600

    Add animated 2x2 elevator door

--- src/main/java/coaltrain/elevator/block/ElevatorDoorBlock.java
+++ src/main/java/coaltrain/elevator/block/ElevatorDoorBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
@@ -25,6 +26,8 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
 import net.minecraft.world.tick.ScheduledTickView;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import org.jetbrains.annotations.Nullable;
 
 public class ElevatorDoorBlock extends BlockWithEntity {
@@ -41,9 +44,16 @@ public class ElevatorDoorBlock extends BlockWithEntity {
 
     private final DoorPart doorPart;
 
+    private static final VoxelShape CLOSED_SHAPE = Block.createCuboidShape(0, 0, 0, 16, 16, 16);
+
     public ElevatorDoorBlock(DoorPart part, Settings settings) {
         super(settings);
         this.doorPart = part;
+        this.setDefaultState(this.stateManager.getDefaultState()
+                .with(PART, part)
+                .with(FACING, Direction.NORTH)
+                .with(HALF, VerticalHalf.BOTTOM)
+                .with(OPEN, false));
     }
 
     @Override
@@ -67,49 +77,54 @@ public class ElevatorDoorBlock extends BlockWithEntity {
         World world = ctx.getWorld();
         Direction facing = ctx.getHorizontalPlayerFacing();
 
-        if (pos.getY() < world.getHeight() - 1) {
-            BlockPos abovePos = pos.up();
-            if (world.getBlockState(abovePos).canReplace(ctx)) {
-                return this.getStateManager().getDefaultState()
-                        .with(PART, doorPart)
-                        .with(FACING, facing)
-                        .with(HALF, VerticalHalf.BOTTOM)
-                        .with(OPEN, false);
-            }
+        if (pos.getY() >= world.getHeight() - 1) {
+            return null;
         }
-        return null;
+
+        BlockPos topPos = pos.up();
+        BlockPos otherBottom = getOppositeBottomPos(pos, facing, doorPart);
+        BlockPos otherTop = otherBottom.up();
+
+        if (!world.getBlockState(topPos).canReplace(ctx)) {
+            return null;
+        }
+        if (!world.getBlockState(otherBottom).canReplace(ctx)) {
+            return null;
+        }
+        if (!world.getBlockState(otherTop).canReplace(ctx)) {
+            return null;
+        }
+
+        return this.getStateManager().getDefaultState()
+                .with(PART, doorPart)
+                .with(FACING, facing)
+                .with(HALF, VerticalHalf.BOTTOM)
+                .with(OPEN, false);
     }
 
     @Override
     public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
-        if (!world.isClient) {
-            Direction facing = state.get(FACING);
-            BlockPos otherBottomPos;
-
-            if (doorPart == DoorPart.LEFT) {
-                otherBottomPos = pos.offset(facing.rotateYClockwise());
-            } else {
-                otherBottomPos = pos.offset(facing.rotateYCounterclockwise());
-            }
-
-            BlockPos topPos = pos.up();
-            BlockPos otherTopPos = otherBottomPos.up();
-
-            world.setBlockState(topPos, state.with(HALF, VerticalHalf.TOP), Block.NOTIFY_ALL);
-
-            Block otherBlock = doorPart == DoorPart.LEFT ? ModBlocks.ELEVATOR_DOOR_RIGHT : ModBlocks.ELEVATOR_DOOR_LEFT;
-            BlockState otherDefaultState = otherBlock.getStateManager().getDefaultState();
-            world.setBlockState(otherBottomPos, otherDefaultState
-                    .with(PART, doorPart == DoorPart.LEFT ? DoorPart.RIGHT : DoorPart.LEFT)
-                    .with(FACING, facing)
-                    .with(HALF, VerticalHalf.BOTTOM)
-                    .with(OPEN, false), Block.NOTIFY_ALL);
-            world.setBlockState(otherTopPos, otherDefaultState
-                    .with(PART, doorPart == DoorPart.LEFT ? DoorPart.RIGHT : DoorPart.LEFT)
-                    .with(FACING, facing)
-                    .with(HALF, VerticalHalf.TOP)
-                    .with(OPEN, false), Block.NOTIFY_ALL);
+        if (world.isClient || state.get(HALF) != VerticalHalf.BOTTOM || doorPart != DoorPart.LEFT) {
+            return;
         }
+
+        Direction facing = state.get(FACING);
+        BlockPos otherBottomPos = getOppositeBottomPos(pos, facing, doorPart);
+        BlockPos topPos = pos.up();
+        BlockPos otherTopPos = otherBottomPos.up();
+
+        world.setBlockState(topPos, state.with(HALF, VerticalHalf.TOP), Block.NOTIFY_ALL);
+
+        Block otherBlock = doorPart == DoorPart.LEFT ? ModBlocks.ELEVATOR_DOOR_RIGHT : ModBlocks.ELEVATOR_DOOR_LEFT;
+        DoorPart otherPart = doorPart == DoorPart.LEFT ? DoorPart.RIGHT : DoorPart.LEFT;
+        BlockState otherState = otherBlock.getStateManager().getDefaultState()
+                .with(PART, otherPart)
+                .with(FACING, facing)
+                .with(HALF, VerticalHalf.BOTTOM)
+                .with(OPEN, false);
+
+        world.setBlockState(otherBottomPos, otherState, Block.NOTIFY_ALL);
+        world.setBlockState(otherTopPos, otherState.with(HALF, VerticalHalf.TOP), Block.NOTIFY_ALL);
     }
 
     @Override
@@ -129,30 +144,21 @@ public class ElevatorDoorBlock extends BlockWithEntity {
         BlockPos bottomPos = half == VerticalHalf.TOP ? pos.down() : pos;
         BlockPos topPos = half == VerticalHalf.BOTTOM ? pos.up() : pos;
 
-        BlockPos otherBottomPos;
-        if (part == DoorPart.LEFT) {
-            otherBottomPos = bottomPos.offset(facing.rotateYClockwise());
-        } else {
-            otherBottomPos = bottomPos.offset(facing.rotateYCounterclockwise());
-        }
+        BlockPos otherBottomPos = part == DoorPart.LEFT
+                ? bottomPos.offset(facing.rotateYClockwise())
+                : bottomPos.offset(facing.rotateYCounterclockwise());
         BlockPos otherTopPos = otherBottomPos.up();
 
-        world.setBlockState(bottomPos, world.getBlockState(bottomPos).with(OPEN, !isOpen), Block.NOTIFY_ALL);
-        world.setBlockState(topPos, world.getBlockState(topPos).with(OPEN, !isOpen), Block.NOTIFY_ALL);
-        world.setBlockState(otherBottomPos, world.getBlockState(otherBottomPos).with(OPEN, !isOpen), Block.NOTIFY_ALL);
-        world.setBlockState(otherTopPos, world.getBlockState(otherTopPos).with(OPEN, !isOpen), Block.NOTIFY_ALL);
+        boolean newState = !isOpen;
 
-        if (world.getBlockEntity(bottomPos) instanceof ElevatorDoorBlockEntity be) {
-            be.toggleAnimation();
-        }
-        if (world.getBlockEntity(topPos) instanceof ElevatorDoorBlockEntity be) {
-            be.toggleAnimation();
-        }
-        if (world.getBlockEntity(otherBottomPos) instanceof ElevatorDoorBlockEntity be) {
-            be.toggleAnimation();
-        }
-        if (world.getBlockEntity(otherTopPos) instanceof ElevatorDoorBlockEntity be) {
-            be.toggleAnimation();
+        setDoorOpen(world, bottomPos, newState);
+        setDoorOpen(world, topPos, newState);
+        setDoorOpen(world, otherBottomPos, newState);
+        setDoorOpen(world, otherTopPos, newState);
+
+        BlockPos basePos = getLeftBottomPos(bottomPos, facing, part);
+        if (world.getBlockEntity(basePos) instanceof ElevatorDoorBlockEntity doorBlockEntity) {
+            doorBlockEntity.setOpenState(newState);
         }
     }
 
@@ -166,12 +172,9 @@ public class ElevatorDoorBlock extends BlockWithEntity {
             BlockPos bottomPos = half == VerticalHalf.TOP ? pos.down() : pos;
             BlockPos topPos = half == VerticalHalf.BOTTOM ? pos.up() : pos;
 
-            BlockPos otherBottomPos;
-            if (part == DoorPart.LEFT) {
-                otherBottomPos = bottomPos.offset(facing.rotateYClockwise());
-            } else {
-                otherBottomPos = bottomPos.offset(facing.rotateYCounterclockwise());
-            }
+            BlockPos otherBottomPos = part == DoorPart.LEFT
+                    ? bottomPos.offset(facing.rotateYClockwise())
+                    : bottomPos.offset(facing.rotateYCounterclockwise());
             BlockPos otherTopPos = otherBottomPos.up();
 
             dropStack(world, pos, new ItemStack(ModItems.ELEVATOR_DOOR));
@@ -197,6 +200,18 @@ public class ElevatorDoorBlock extends BlockWithEntity {
             }
         }
 
+        if (direction.getAxis().isHorizontal()) {
+            Direction facing = state.get(FACING);
+            DoorPart part = state.get(PART);
+            Direction expectedNeighbor = part == DoorPart.LEFT
+                    ? facing.rotateYClockwise()
+                    : facing.rotateYCounterclockwise();
+
+            if (direction == expectedNeighbor && !(neighborState.getBlock() instanceof ElevatorDoorBlock)) {
+                return Blocks.AIR.getDefaultState();
+            }
+        }
+
         return state;
     }
 
@@ -211,14 +226,47 @@ public class ElevatorDoorBlock extends BlockWithEntity {
     @Nullable
     @Override
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new ElevatorDoorBlockEntity(pos, state);
+        return hasBlockEntity(state) ? new ElevatorDoorBlockEntity(pos, state) : null;
     }
 
     @Nullable
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
+        if (!hasBlockEntity(state)) {
+            return null;
+        }
         return validateTicker(type, ModBlockEntities.ELEVATOR_DOOR_BLOCK_ENTITY,
                 (world1, pos, state1, blockEntity) -> blockEntity.tick(world1, pos, state1));
+    }
+
+    @Override
+    public boolean hasBlockEntity(BlockState state) {
+        return state.get(PART) == DoorPart.LEFT && state.get(HALF) == VerticalHalf.BOTTOM;
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, WorldView world, BlockPos pos) {
+        return state.get(OPEN) ? VoxelShapes.empty() : CLOSED_SHAPE;
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, WorldView world, BlockPos pos, ShapeContext context) {
+        return state.get(OPEN) ? VoxelShapes.empty() : CLOSED_SHAPE;
+    }
+
+    private static BlockPos getOppositeBottomPos(BlockPos pos, Direction facing, DoorPart part) {
+        return part == DoorPart.LEFT ? pos.offset(facing.rotateYClockwise()) : pos.offset(facing.rotateYCounterclockwise());
+    }
+
+    private static BlockPos getLeftBottomPos(BlockPos bottomPos, Direction facing, DoorPart part) {
+        return part == DoorPart.LEFT ? bottomPos : bottomPos.offset(facing.rotateYCounterclockwise());
+    }
+
+    private static void setDoorOpen(World world, BlockPos pos, boolean open) {
+        BlockState state = world.getBlockState(pos);
+        if (state.getBlock() instanceof ElevatorDoorBlock) {
+            world.setBlockState(pos, state.with(OPEN, open), Block.NOTIFY_LISTENERS);
+        }
     }
 
     public enum DoorPart implements StringIdentifiable {
